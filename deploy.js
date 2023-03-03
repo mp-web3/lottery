13118613cecc8da623ca00d666b982291da270c2
mp-web3 <mp.web3.t@gmail.com>
2023-03-03 22:33:14 +0100

    Wrote some tests to se if contract deployes

--- deploy.js
+++ deploy.js
@@ -3,9 +3,9 @@ const Web3 = require('web3');
 const { interface, bytecode } = require('./compile');
 
 const provider = new HDWalletProvider(
-  'REPLACE_WITH_YOUR_MNEMONIC',
+  'current hedgehog orphan hire below fuel bargain park fuel faculty favorite leisure',
   // remember to change this to your own phrase!
-  'https://rinkeby.infura.io/v3/15c1d32581894b88a92d8d9e519e476c'
+  'https://goerli.infura.io/v3/31a7018b289c46da9512a2662d07f455'
   // remember to change this to your own endpoint!
 );
 const web3 = new Web3(provider);
