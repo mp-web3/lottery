b539b3eda7df40e49bd7e86d3d7885177a41b3ae
mp-web3 <mp.web3.t@gmail.com>
2023-03-04 19:55:53 +0100

    send money to winner and reset players array TEST

--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -97,4 +97,34 @@ describe('Lottery Contract', () => {
             from: accounts[0]
         });
     });
+
+    it('sends money to the winner and resets player array', async () => {
+        await lottery.methods.enter().send({
+            from: accounts[0],
+            value: web3.utils.toWei('2', 'ether')
+        });
+
+        const initialBalance = await web3.eth.getBalance(accounts[0]);
+
+        await lottery.methods.pickWinner().send({ from: accounts[0] });
+
+        const finalBalance = await web3.eth.getBalance(accounts[0]);
+
+        const difference = finalBalance - initialBalance;
+        console.log(finalBalance - initialBalance);
+        assert(difference > web3.utils.toWei('1.8', 'ether'));
+
+        // check that player array is empty
+        const players = await lottery.methods.getPlayers().call({
+            from: accounts[0]
+        });
+        assert.equal(0, players.length);
+
+        const contractBalance = await web3.eth.getBalance(lottery.options.address);
+        assert.equal(0, contractBalance);
+    });
+
+
+
+
 });
