33f9abff3dd0b95090c163fe7a95e14384012863
mp-web3 <mp.web3.t@gmail.com>
2023-03-04 09:16:44 +0100

    run some tests

--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -7,7 +7,7 @@ const { interface, bytecode } = require('../compile');
 let lottery;
 let accounts;
 
-beforeEach( async () => {
+beforeEach( async () => { //Before we run each test it resets the accounts
     accounts = await web3.eth.getAccounts();
 
     lottery = await new web3.eth.Contract(JSON.parse(interface))
@@ -21,7 +21,7 @@ describe('Lottery Contract', () => {
     });
 
     it('allows one account to enter', async () => {
-        await lottery.methods.enter().send({
+        await lottery.methods.enter().send({ // Enter the lottery
             from: accounts[0],
             value: web3.utils.toWei('0.02', 'ether') // Convert ether to Wei 
         });
@@ -33,5 +33,43 @@ describe('Lottery Contract', () => {
         assert.equal(accounts[0], players[0]);
         assert.equal(1, players.length)
     });
+
+    it('allows multiple accounts to enter', async () => {
+        await lottery.methods.enter().send({
+            from: accounts[0],
+            value: web3.utils.toWei('0.02', 'ether')
+        });
+        await lottery.methods.enter().send({
+            from: accounts[1],
+            value: web3.utils.toWei('0.02', 'ether')
+        });
+        await lottery.methods.enter().send({
+            from: accounts[2],
+            value: web3.utils.toWei('0.02', 'ether')
+        });
+
+        const players = await lottery.methods.getPlayers().call({
+            from: accounts[0]
+        });
+
+        assert.equal(accounts[0], players[0]);
+        assert.equal(accounts[1], players[1]);
+        assert.equal(accounts[2], players[2]);
+        assert.equal(3, players.length)
+    });
+
+    it('requires a minimum amount of ether to enter', async () => {
+        try{
+            await lottery.methods.enter().send({
+                from: accounts[4],
+                value: web3.utils.toWei('0.2', 'ether')
+            });
+            assert(false);
+        } catch (err) {
+            assert (err);
+        }
+    });
+
+    it()
 
 });
